8aad9efc31424c767ab76bba03f9d7d33952093b
Charlie-Phillip5 <cp932@exeter.ac.uk>
2026-01-30 10:12:05 +0000

    PrintPattern1 completed

---
+++ PrintPattern1.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+class PrintPattern1{
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Input an integer:");
+        int length = in.nextInt();
+        System.out.println()
+        for(int i = 1; i<=length; i++){
+            for(int n = 1; n<=i; n++){
+                System.out.print(n);
+            }
+            System.out.println();
+        }
+
+    }
+}
